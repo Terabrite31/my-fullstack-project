f554991ce64ea06680ede9b05764ad93f7119e8b
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-03-24 00:45:20 +0800

    fix

--- api/login.js
+++ api/login.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const {email, password} = req.query;
 
   const result = await sql`
-  SELECT password FROM users WHERE email = $(email)
+  SELECT password FROM users WHERE email = ${email}
   `;
 
   if (result.length === 0) {
