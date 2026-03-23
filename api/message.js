f4017071265fcec3ea8e45d9a1f0ee06e2a2eafe
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-03-23 23:15:27 +0800

    update

--- api/message.js
+++
@@ -1,5 +0,0 @@
-import { sql } from '../lib/db';
-
-export default async function handler(req, res) {
-  res.status(200).json({ message: "test" });
-}
---
+++ api/signup.js
@@ -0,0 +1,21 @@
+import { sql } from '../lib/db';
+
+export default async function handler(req, res) {
+
+  const { email, password } = req.query;
+
+  await sql`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      email TEXT,
+      password TEXT
+    )
+  `;
+
+  await sql`
+  INSERT INTO users (email, password)
+    VALUES (${email}, ${password})
+`;
+
+  res.json({ checker: "approved" });
+}
