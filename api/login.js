c5dedd5844f9a23c5d5f724d48c4e66a8ac0a9fb
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-03-24 00:02:31 +0800

    update

--- api/login.js
+++ api/login.js
@@ -1,5 +1,27 @@
 import { sql } from '../lib/db';
 
 export default async function handler(req, res) {
-  res.status(200).json({ message: "test" });
+
+  const {email, password} = req.query;
+
+  const result = await sql`
+  SELECT password FROM users WHERE email = $(email)
+  ';
+
+  if (result.length === 0) {
+  return res.json({ checker: "no user" });
 }
+
+if (password === result[0].password) {
+res.json({ checker: "approved"});
+}
+else {
+  res.json({ checker: "wrong password" });
+}
+
+}
+
+
+
+
+
